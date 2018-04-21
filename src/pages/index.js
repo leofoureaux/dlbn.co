b07a8a46bfcb6892e1253aac71c3593c4eb8939a
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-04-21 19:22:49 -0300

    Remove About page files, move to Index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Typist from 'react-typist';
 import { withPrefix } from 'gatsby-link';
-import About from './about'
+import Badges from '../components/Badges'
 
 import SimpleIcon from '../components/SimpleIcon'
 
@@ -56,7 +56,69 @@ class IndexPage extends React.Component {
               >{messages.tagline.button}</Link>
           </section>
           <div className="separator"></div>
-          <About messages={messages.about} />
+          <div>
+            <h2>{messages.about.title}</h2>
+            <section className="mb-3">
+              {messages.about.description}
+            </section>
+            <h3>{messages.about.tech}</h3>
+            <Badges skills={[
+              {
+                icon: 'react',
+                name: 'React',
+                url: 'https://reactjs.org/',
+                color: '#61DAFB'
+              },
+              {
+                icon: 'redux',
+                name: 'Redux',
+                url: 'https://redux.js.org/',
+                color: '#61DAFB'
+              },
+              {
+                icon: 'nodejs',
+                name: 'Node.js',
+                url: 'https://nodejs.org/',
+                color: '#61DAFB'
+              },
+              {
+                icon: 'webpack',
+                name: 'webpack',
+                url: 'https://webpack.js.org/',
+                color: '#61DAFB'
+              },
+              {
+                icon: 'sass',
+                name: 'Sass',
+                url: 'https://sass-lang.com/',
+                color: '#61DAFB'
+              },
+              {
+                icon: 'gatsby',
+                name: 'Gatsby',
+                url: 'https://gatsbyjs.org/',
+                color: '#61DAFB'
+              },
+              {
+                icon: 'docker',
+                name: 'Docker',
+                url: 'https://gatsbyjs.org/',
+                color: '#61DAFB'
+              },
+              {
+                icon: 'git',
+                name: 'Git',
+                url: 'https://git-scm.com/',
+                color: '#61DAFB'
+              },
+              {
+                icon: 'graphql',
+                name: 'GraphQL',
+                url: 'https://graphql.org/',
+                color: '#61DAFB'
+              },
+            ]} />
+          </div>
         </div>
       )
     }
