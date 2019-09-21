63e476526cf16569384a98953228f9a18682e50a
Mateus Dal Bianco <mateus@dlbn.co>
2019-09-21 17:09:49 +0300

    feat(text): update styled-system/rebass api

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,14 +1,16 @@
 import { Text as RebassText } from 'rebass';
 import styled from 'styled-components';
-import { variant } from 'styled-system';
+import { variant, space, color } from 'styled-system';
 
-const Text = styled(RebassText)(
-  variant({
+const Text = styled(RebassText)`
+  ${variant({
     variants: {
       primary: { color: 'white' },
       secondary: { color: 'silver' },
     },
-  }),
-);
+  })}
+  ${space}
+  ${color}
+`;
 
 export default Text;
