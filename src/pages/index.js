c61fabedb04c3f3eacda9d6003bb711fa78c853a
Mateus Dal Bianco <mateus@dlbn.co>
2019-09-21 14:05:12 +0300

    feat(typer): remove typer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import { fontSize } from 'styled-system';
 import styled from 'styled-components';
 import { Box } from 'rebass';
 
 import Layout from '../components/layout';
 import Avatar from '../components/Avatar';
-import Typer from '../components/Typer';
 import About from '../components/About';
 import Contact from '../components/Contact';
 import Separator from '../components/Separator';
 import Projects from '../components/Projects';
 import Articles from '../components/Articles';
-
-const StyledTyper = styled(Typer)`
-  ${fontSize}
-`;
 
 const Section = styled(Box).attrs(() => ({
   as: 'section',
@@ -25,7 +19,6 @@ const IndexPage = () => (
     <Section mb={4}>
       <Avatar />
     </Section>
-    <Section mb={4}>{/* <StyledTyper fontSize={[4, 5]} /> */}</Section>
     <Section mb={5}>
       <About />
     </Section>
