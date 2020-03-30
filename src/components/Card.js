69db8b7dc846ea26882d4024644722727b064196
Mateus Dal Bianco <mateus@dlbn.co>
2020-03-29 21:01:47 -0300

    feat(card): create card component

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import { Box } from 'rebass';
+import styled from 'styled-components';
+
+const Card = styled(Box)`
+  border: 2px solid #fafafa;
+  box-shadow: 4px 4px 0px 0 #44bce3;
+`;
+
+export default Card;
